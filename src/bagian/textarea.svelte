be0194022c6459706d3390a53bcf669753c8b508
mzaini30 <muhzaini30@gmail.com>
2023-04-04 11:17:56 +0800

    Sudah

--- src/bagian/textarea.svelte
+++ src/bagian/textarea.svelte
@@ -3,12 +3,14 @@
   export let is_readonly = false;
   export let teks;
   export let is_rtl = false;
+  export let ini;
 </script>
 
 <textarea
   dir={is_rtl ? "rtl" : "ltr"}
   readonly={is_readonly}
   {placeholder}
+  bind:this={ini}
   on:click={is_readonly ? this.select() : null}
   bind:value={teks}
   class="w-full h-40 focus:outline-none p-2 rounded border-2 border-black"
--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -5,16 +5,41 @@
 
   let latin;
   let arab;
+  let bagian_arab;
+  let bagian_latin;
+
+  if (localStorage.latin) {
+    latin = localStorage.latin;
+  }
+  if (localStorage.arab) {
+    arab = localStorage.arab;
+  }
 
   $: if (latin && latin.length > 0) {
     arab = ubah_jadi_arab(latin);
+    localStorage.latin = latin;
+    localStorage.arab = arab;
+    bagian_arab.scrollTop = bagian_arab.scrollHeight;
   } else {
     arab = "";
+    localStorage.latin = "";
+    localStorage.arab = "";
   }
 </script>
 
 <div class="p-2 grid grid-cols-1 gap-2">
-  <Textarea bind:teks={arab} is_rtl is_readonly placeholder="الحصيل هنا" />
-  <Textarea bind:teks={latin} placeholder="Tulis huruf latin di sini" />
+  <Textarea
+    bind:this={bagian_arab}
+    bind:teks={arab}
+    is_rtl
+    bind:ini={bagian_arab}
+    is_readonly
+    placeholder="الحصيل هنا"
+  />
+  <Textarea
+    bind:teks={latin}
+    bind:ini={bagian_latin}
+    placeholder="Tulis huruf latin di sini"
+  />
   <a href="https://wa.me/6281545143654"><img src={iklan} alt="" /></a>
 </div>
